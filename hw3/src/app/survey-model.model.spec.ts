d7d6ca7b70d784cfcdba0e2d4a3ba4d846a45390
Kelvin-T-Lu <61221557+Kelvin-T-Lu@users.noreply.github.com>
2023-07-16 17:49:09 -0400

    added survey model

---
+++ hw3/src/app/survey-model.model.spec.ts
@@ -0,0 +1,7 @@
+import { SurveyModel } from './survey-model.model';
+
+describe('SurveyModel', () => {
+  it('should create an instance', () => {
+    expect(new SurveyModel()).toBeTruthy();
+  });
+});
